56cdfb90577de45f93c5470d4c7256b0a6310273
Dr-De-coder <sachinsums08@gmail.com>
2024-11-11 14:18:10 +0000

    datatype summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+//primitive 
+
+// 7 types : String, Number Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const is LoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumbwr = 3456543576654356754n
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+let myObj = {
+    mame: "hitesh",
+    age: 22,
+}
+
+const muFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof bigNumbwr);
